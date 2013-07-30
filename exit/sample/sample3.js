7f1091080576a6d1f4e30563277bc09927725dcf
Jeon Jeong Ho <jeonjeongho@Jeon-ui-MacBook-Pro.local>
2013-07-30 14:48:54 +0900

    샘플3 코멘트 좀 달아서 다시 커밋

--- exit/sample/sample3.js
+++ exit/sample/sample3.js
@@ -7,9 +7,10 @@ var client = mysql.createConnection({
 	
 	host : 'localhost',
 	port : '3306',
+// DB 및 USER / ID 변경
 	user : 'exit',
 	password : 'test',
-	database : 'exit_proj'
+	database : 'exit_proj' 
 	
 	
 });
@@ -27,7 +28,7 @@ client.connect(function(err){
 });
 
 function insertData(){
-	
+// PRIMARY-KEY 이므로 다시 추가시 에러 발생 가능성 농후	
 	client.query('INSERT INTO user SET ?',{
 		//storeid : 1,
 		email: 'shai0827@gmail.com',
